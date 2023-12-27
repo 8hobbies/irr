849a6cd8da6f6ab9ca5b4218382788f0428b02ba
Hong Xu <hong@topbug.net>
2023-12-27 14:37:47 -0800

    Add tests for two and three inputs

--- index.test.ts
+++ index.test.ts
@@ -8,4 +8,16 @@ describe('computeIrr function', () => {
   test('one input', () => {
     expect(computeIrr([1])).toStrictEqual([])
   })
+  test('two solvable inputs', () => {
+    expect(computeIrr([-1, 2])).toStrictEqual([1])
+  })
+  test('two unsolvable inputs', () => {
+    expect(computeIrr([-1, -1])).toStrictEqual([])
+  })
+  test('three solvable inputs', () => {
+    expect(computeIrr([-1, 1, 2])).toStrictEqual([1])
+  })
+  test('three unsolvable inputs', () => {
+    expect(computeIrr([-1, -1, -2])).toStrictEqual([])
+  })
 })
