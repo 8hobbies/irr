0de69895f554d98d6a58c7e5b925146a445703af
Hong Xu <hong@topbug.net>
2023-12-27 12:36:55 -0800

    Add Emacs configuration

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil .
+      ((eval .
+             (add-to-list 'exec-path
+                          (concat (locate-dominating-file default-directory dir-locals-file)
+                                  "node_modules/.bin/"))))))
