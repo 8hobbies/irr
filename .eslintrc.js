12bd00a34b529c457a12307b425333264994e170
Hong Xu <hong@topbug.net>
2023-12-26 21:50:37 -0800

    Use es 2021

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2020": true,
+        "es2021": true,
         "node": true
     },
     "extends": "standard-with-typescript",
