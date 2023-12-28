6d5337746fd3b54eb710070a84f94e30c21262b0
Hong Xu <hong@topbug.net>
2023-12-27 17:53:15 -0800

    Require 100% coverage

--- jest.config.ts
+++ jest.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage'
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -44,7 +44,14 @@ const config: Config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
